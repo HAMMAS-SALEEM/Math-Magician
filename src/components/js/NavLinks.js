e41a5997c8dd18d6db1ad25d2b9e702a25ead37a
HAMMAS-SALEEM <hammassaleem376@gmail.com>
2022-12-17 13:31:48 +0500

    Add Icons to Navbar Links

--- src/components/js/NavLinks.js
+++ src/components/js/NavLinks.js
@@ -1,13 +1,25 @@
 import '../css/Header.css';
 import { Link } from 'react-router-dom';
+import { ReactComponent as Home } from '../images/icons/home.svg';
+import { ReactComponent as Calculator } from '../images/icons/calculator.svg';
+import { ReactComponent as Quote } from '../images/icons/quote.svg';
 
 const NavLinks = () => (
   <header>
     <h1 className="navbar-heading nav-item-color">Math Magician</h1>
     <nav className="nav-links">
-      <Link className="nav-item nav-item-color single-nav-link" to="/">Home</Link>
-      <Link className="nav-item nav-item-color single-nav-link" to="/calculator">Calculator</Link>
-      <Link className="nav-item nav-item-color single-nav-link" to="/quote">Quote</Link>
+      <Link className="nav-item nav-item-color single-nav-link" to="/">
+        <Home />
+        <span>Home</span>
+      </Link>
+      <Link className="nav-item nav-item-color single-nav-link" to="/calculator">
+        <Calculator />
+        <span>Calculator</span>
+      </Link>
+      <Link className="nav-item nav-item-color single-nav-link" to="/quote">
+        <Quote />
+        <span>Quote</span>
+      </Link>
     </nav>
   </header>
 );
